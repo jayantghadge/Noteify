78ff5ec0aadef068cd943b37d10d5962a14b23dc
Jayant Ghadge <jayantghadge6@gmail.com>
2023-12-30 22:51:41 +0530

    Fixed position of Social icons in Footer compnonent

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,21 +5,23 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="flex justify-center">
-        <p className="text-sm leading-5">
+        <p className="mx-auto text-sm leading-5">
           Developed by <b>Jayant Ghadge</b>
           <br />
-          <a target="_blank" href="https://twitter.com/JayantGhadge">
-            <FaTwitter className="social-media-icons text-blue-500" />
-          </a>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/jayant-ghadge-700739181"
-          >
-            <FaLinkedin className="social-media-icons text-blue-500 " />
-          </a>
-          <a target="_blank" href="https://github.com/jayantghadge">
-            <FaGithub className="social-media-icons" />
-          </a>
+          <div className="flex justify-center">
+            <a target="_blank" href="https://twitter.com/JayantGhadge">
+              <FaTwitter className="social-media-icons text-blue-500 " />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/jayant-ghadge-700739181"
+            >
+              <FaLinkedin className="social-media-icons text-blue-500 " />
+            </a>
+            <a target="_blank" href="https://github.com/jayantghadge">
+              <FaGithub className="social-media-icons" />
+            </a>
+          </div>
         </p>
       </div>
     </footer>
